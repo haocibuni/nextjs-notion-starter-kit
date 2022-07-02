418a4cdfa3321d591f0958041e3128246866ad63
Lei Wu <1723541415@qq.com>
2022-07-03 00:42:20 +0800

    update notionid

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ee9bdf63274540f6af4e7f9ae3651ec3?v=b1e2c16873e04c3db49da028ea785a7c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
